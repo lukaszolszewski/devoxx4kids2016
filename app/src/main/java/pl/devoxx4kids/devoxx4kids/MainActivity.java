7e543d1f6c27b13040ffebf1852596faeb5c7852
Lukasz Olszewski <asdluki@gmail.com>
2016-08-19 14:28:11 +0200

    estimote. init.

--- app/src/main/java/pl/devoxx4kids/devoxx4kids/MainActivity.java
+++ app/src/main/java/pl/devoxx4kids/devoxx4kids/MainActivity.java
@@ -2,12 +2,55 @@ package pl.devoxx4kids.devoxx4kids;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.EstimoteSDK;
+import com.estimote.sdk.Nearable;
+import com.estimote.sdk.Region;
+
+import java.util.List;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BeaconManager beaconManager;
+    private String scanId;
+    private String TAG = "devoxx4kids";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beaconManager = new BeaconManager(this);
+
+        beaconManager.setNearableListener(new BeaconManager.NearableListener() {
+            @Override public void onNearablesDiscovered(List<Nearable> nearables) {
+                Log.d(TAG, "Discovered nearables: " + nearables);
+            }
+        });
+
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> beacons) {
+                Log.d(TAG, "Discovered beacons : " + beacons.size());
+            }
+        });
+
+        final Region defaultUUIDRegion = new Region("Beacons with default Estimote UUID",
+                UUID.fromString("2f6578fe897b657231f40ae9e5b19d26"), null, null);
+
+        // Should be invoked in #onStart.
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override public void onServiceReady() {
+                //scanId = beaconManager.startNearableDiscovery();
+
+                beaconManager.startRanging(defaultUUIDRegion);
+            }
+        });
+
     }
 }
