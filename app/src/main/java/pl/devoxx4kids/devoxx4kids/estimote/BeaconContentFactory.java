3d42d0315a5ee8488f3f7d503a99a9d927bcf637
Lukasz Olszewski <asdluki@gmail.com>
2016-09-05 21:44:06 +0200

    Proximity Content

---
+++ app/src/main/java/pl/devoxx4kids/devoxx4kids/estimote/BeaconContentFactory.java
@@ -0,0 +1,10 @@
+package pl.devoxx4kids.devoxx4kids.estimote;
+
+public interface BeaconContentFactory {
+
+    void getContent(BeaconID beaconID, Callback callback);
+
+    interface Callback {
+        void onContentReady(Object content);
+    }
+}
